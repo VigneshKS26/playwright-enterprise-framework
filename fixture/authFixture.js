3235ff24581799404ef5a9e0fe293d5765408dea
VigneshKS-QA <vigneshkssomu26@gmail.com>
2026-08-14 18:04:04 +0530

    Debug

--- fixture/authFixture.js
+++ fixture/authFixture.js
@@ -9,6 +9,8 @@ export const test = base.extend({
     const loginPage = new LoginPage(page);
     const depositPage = new DepositPage(page);
     await page.goto(process.env.MEMBER_URL);
+    console.log("Current URL:", this.page.url());
+    await page.screenshot({ path: "debug.png" });
     await use({
       context,
       page,
@@ -23,6 +25,8 @@ export const test = base.extend({
     const loginPage = new LoginPage(page);
     const depositPage = new DepositPage(page);
     await page.goto(process.env.MERCHANT_URL);
+    console.log("Current URL:", this.page.url());
+    await page.screenshot({ path: "debug1.png" });
     await use({
       context,
       page,
--- fixture/fixture.js
+++ fixture/fixture.js
@@ -11,7 +11,8 @@ export const test = base.extend({
     const depositPage = new DepositPage(page);
 
     await page.goto(process.env.MEMBER_URL);
-
+    console.log("Current URL:", this.page.url());
+    await page.screenshot({ path: "debug1.png" });
     await loginPage.loginMember();
 
     await use({
@@ -31,6 +32,8 @@ export const test = base.extend({
     const loginPage = new LoginPage(page);
     const depositPage = new DepositPage(page);
     await page.goto(process.env.MERCHANT_URL);
+    console.log("Current URL:", this.page.url());
+    await page.screenshot({ path: "debug.png" });
     await use({
       context,
       page,
